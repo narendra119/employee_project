572ab33151f5b6c8fc9415898b79157b5439e523
reddikumar55 <reddikumarmallela@gmail.com>
2021-06-03 14:57:04 +0530

    added city model

--- user_management/models.py
+++ user_management/models.py
@@ -10,11 +10,21 @@ class Employee(models.Model):
         ('F', 'FEMALE'),
         ('O', 'OTHER')
     )
+    
     emp_id = models.IntegerField(blank=False, unique=True)
     name = models.CharField(max_length=255)
     age = models.IntegerField()
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
-
+    
     def __str__(self):
         return self.name
 
+class City(models.Model):
+    METRO_OPT=(
+        ('y','yes'),
+        ('n','no')
+    )
+    
+    name=models.CharField(max_length=100)
+    metro=models.CharField(max_length=1, choices=METRO_OPT)
+    
