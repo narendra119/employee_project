1206fa9d338947fbe49593436eca2d36544ba5ad
local <manojkumar112131@gmail.com>
2021-06-03 15:36:07 +0530

    changed models.py

--- user_management/admin.py
+++ user_management/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from user_management.models import Employee
+from user_management.models import Organisation,employee_Details
 
 # Register your models here.
 admin.site.register(Employee)
+admin.site.register(Organisation)
+admin.site.register(employee_Details)
--- user_management/models.py
+++ user_management/models.py
@@ -18,3 +18,17 @@ class Employee(models.Model):
     def __str__(self):
         return self.name
 
+class Organisation(models.Model):
+    headQuarters=models.CharField(max_length=50)
+    branch=models.CharField(max_length=20)
+    branch_Address=models.TextField(max_length=50)
+    employee_ID=models.IntegerField(null=True)
+    employee_Name=models.CharField(max_length=20,null=True)
+    performance=models.CharField(max_length=40,null=True)
+ 
+class Employee_Details(models.Model):
+    Organisation=models.ForeignKey(Organisation,on_delete=models.CASCADE)
+class Meta:
+    order_with_respect_to='Organisation'
+    def __str__ (self):
+        return self.employee_Name
