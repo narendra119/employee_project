8e4d2089b2da930814cd9b4a7148b4f357d29ce2
sandytony <vayalpatisandeep@gmail.com>
2021-06-10 22:15:57 +0530

    nested Serializer

--- user_management/serializers.py
+++ user_management/serializers.py
@@ -1,5 +1,11 @@
 from rest_framework import serializers
 
+class OrganisationSerializer(serializers.Serializer):
+    
+    headQuarters = serializers.CharField(max_length=255)
+    branch = serializers.CharField(max_length=255)
+
+
 class EmployeeSerializer(serializers.Serializer):
     GENDER_CHOICES = (
         ('M', 'MALE'),
@@ -9,6 +15,11 @@ class EmployeeSerializer(serializers.Serializer):
     emp_id = serializers.IntegerField()
     name = serializers.CharField(max_length=255)
     age = serializers.IntegerField()
+    organisation = OrganisationSerializer()
+    dob = serializers.SerializerMethodField()
     # gender = serializers.CharField(max_length=1)
+    
+    def get_dob(self,obj):
+        return obj.name + " " + obj.age
     
 
